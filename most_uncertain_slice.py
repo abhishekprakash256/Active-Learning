64f9b0a1770e73d0b5ffd3dd5a24f2ca88b7cbb6
abhishekprakash256 <abhishekprakash47@gmail.com>
2022-09-19 08:57:16 -0400

    files

--- most_uncertain_slice.py
+++ most_uncertain_slice.py
@@ -10,10 +10,10 @@ import time
 #make the numpy array 
 
 global array_1 
-array_1= np.random.randint(low=0,high=1, size = (4,4) ,dtype=int)
+array_1= np.random.randint(low=0,high=2, size = (4,4) ,dtype=int)
 
 global array_2 
-array_2 = np.random.randint(low=0,high=1, size = (4,4) ,dtype=int)
+array_2 = np.random.randint(low=0,high=2, size = (4,4) ,dtype=int)
 
 
 
@@ -26,19 +26,25 @@ def most_uncertain_values():
 	Return:
 		row, cloumn (int): the colum and row width max uncertain values 
 	"""
-
-	print(array_1)
 	shape1, shape2 = array_1.shape
 
+	holes_arry = np.zeros((shape1,shape2))
+
+	count_rows = 0
+	count_coloumns = 0
+	max_rows = 0
+	max_columns = 0
 	for i in range(shape1):
 		for j in range(shape1):
 			#add the condition for the equal checking 
 			if array_1[i,j] == array_2[i,j]:
-				continue
-			print(array_1[i,j])
+				holes_arry[i,j] = array_1[i,j]
+			else:
+				holes_arry[i,j] = -1
 
 
-	return done
+
+	return holes_arry
 
 
 if __name__ == '__main__':
